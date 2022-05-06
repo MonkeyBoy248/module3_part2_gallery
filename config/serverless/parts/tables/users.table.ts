a03b3b3b6cabb58d37876ace8244a5d172a1ab28
Kirill Temnyi <kirik201100@mail.ru>
2022-05-06 14:12:49 +0300

    feat: create empty DynamoDB table for users and pictures data
    MODULE-3-PART-2

---
+++ config/serverless/parts/tables/users.table.ts
@@ -0,0 +1,120 @@
+import {AWSPartitial} from "../../types";
+
+export const usersTableConfig: AWSPartitial = {
+  provider: {
+    environment: {
+      USERS_TABLE_NAME: '${self:custom.tablesNames.UsersTable.${self:provider.stage}}',
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: 'Allow',
+            Action: [
+              'dynamodb:DescribeTable',
+              'dynamodb:Query',
+              'dynamodb:Scan',
+              'dynamodb:GetItem',
+              'dynamodb:PutItem',
+              'dynamodb:DeleteItem',
+              'dynamodb:UpdateItem',
+              'dynamodb:BatchGetItem',
+              'dynamodb:BatchWriteItem',
+            ],
+            Resource: [
+              'arn:aws:dynamodb:*:*:table/${self:custom.tablesNames.UsersTable.${self:provider.stage}}',
+              'arn:aws:dynamodb:*:*:table/${self:custom.tablesNames.UsersTable.${self:provider.stage}}/index/*',
+            ],
+          },
+        ],
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      UsersTable: {
+        Type: 'AWS::DynamoDB::Table',
+        DeletionPolicy: 'Retain',
+        Properties: {
+          AttributeDefinitions: [
+            {
+              AttributeName: 'Partition Key: Email',
+              AttributeType: 'S'
+            },
+            {
+              AttributeName: 'Sort Key: PictureName',
+              AttributeType: 'S'
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'Partition Key: Email',
+              KeyType: 'HASH',
+            },
+            {
+              AttributeName: 'Sort Key: PictureName',
+              KeyType: 'RANGE',
+            },
+          ],
+          // GlobalSecondaryIndexes: [
+          //   {
+          //     IndexName: 'ProducerIdGlobalIndex',
+          //     KeySchema: [
+          //       {
+          //         AttributeName: 'Partition Key: PK',
+          //         KeyType: 'HASH',
+          //       },
+          //       {
+          //         AttributeName: 'Sort Key: SK',
+          //         KeyType: 'RANGE',
+          //       },
+          //     ],
+          //     Projection: {
+          //       ProjectionType: 'ALL',
+          //     },
+          //     ProvisionedThroughput: {
+          //       ReadCapacityUnits: 2,
+          //       WriteCapacityUnits: 2,
+          //     },
+          //   },
+          //   {
+          //     IndexName: 'CrewIdGlobalIndex',
+          //     KeySchema: [
+          //       {
+          //         AttributeName: 'crewId',
+          //         KeyType: 'HASH',
+          //       },
+          //       {
+          //         AttributeName: 'status',
+          //         KeyType: 'RANGE',
+          //       },
+          //     ],
+          //     Projection: {
+          //       ProjectionType: 'ALL',
+          //     },
+          //     ProvisionedThroughput: {
+          //       ReadCapacityUnits: 2,
+          //       WriteCapacityUnits: 2,
+          //     },
+          //   },
+          // ],
+          BillingMode: 'PAY_PER_REQUEST',
+          TableName: '${self:custom.tablesNames.UsersTable.${self:provider.stage}}',
+          StreamSpecification: {
+            StreamViewType: 'NEW_AND_OLD_IMAGES',
+          },
+        },
+      },
+    },
+  },
+  custom: {
+    tablesNames: {
+      UsersTable: {
+        local: 'Users-pictures-local',
+        dev: 'Users-pictures-dev',
+        test: 'Users-pictures-test',
+        prod: 'Users-pictures',
+      },
+    },
+  },
+};
