a1caac32dea1ab8215f19c438a1ee784297cfe93
Kirill Temnyi <kirik201100@mail.ru>
2022-05-17 14:05:50 +0300

    refactor: change lambdas timeout value from 6 to 30
    MODULE-3-PART-2

--- config/serverless/parts/authConfig/index.ts
+++ config/serverless/parts/authConfig/index.ts
@@ -1,7 +1,7 @@
 export const signUp = {
   handler: "api/authentication/handler.signUp",
   description: "SignUp users",
-  timeout: 6,
+  timeout: 30,
   memorySize: 128,
   events: [
     {
@@ -16,7 +16,7 @@ export const signUp = {
 export const logIn = {
   handler: "api/authentication/handler.logIn",
   description: "LogIn user",
-  timeout: 6,
+  timeout: 30,
   memorySize: 128,
   events: [
     {
@@ -28,24 +28,9 @@ export const logIn = {
   ]
 };
 
-export const uploadDefaultUsers = {
-  handler: "api/authentication/handler.uploadDefaultUsers",
-  description: "Upload default users to the DB",
-  timeout: 6,
-  memorySize: 128,
-  events: [
-    {
-      httpApi: {
-        path: "/auth/upload-default-users",
-        method: "post"
-      }
-    }
-  ]
-};
-
 export const httpApiJwtAuthorizer = {
   handler: "api/authentication/handler.authenticate",
   description: "Authenticate user",
-  timeout: 6,
+  timeout: 30,
   memorySize: 128
 };
